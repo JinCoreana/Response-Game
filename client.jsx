7df288c446ece14467626fd7dbc36a1e244417a1
Seoyeonjin Choi <itmustbejin@gmail.com>
2022-01-27 18:52:09 +0100

    First commit
    The basic structure has beem set up.
    I will update the style and readme.

---
+++ client.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import ResponseGame from './response_game'
+
+ReactDOM.render(<ResponseGame/>, document.querySelector('#root'))
---
+++ response_game.jsx
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+
+
+class ResponseGame extends Component {
+    state = {
+        state: 'waiting',
+        message: 'Click to start the game',
+        result: [],
+    }
+timeout;
+startTime;
+endTime;
+     onClickScreen = () => {
+     const {state, message, result} = this.state;
+     if (state === 'waiting') {
+         this.setState({
+             state:'ready',
+             message: 'Get ready and give a click on green'
+         })
+         this.timeout = setTimeout(()=> {
+             this.setState({
+                 state:'now',
+                 Message: 'Right now!'
+             })
+             this.startTime = new Date();
+           
+            }, Math.floor(Math.random() * 1000) + 2000);
+        
+         } else if (state === 'ready') {
+            clearTimeout(this.timeout)
+            this.setState({
+                state:'waiting',
+               message: 'Boo! Too fast! Wait for Green'
+            })
+            
+         }
+         else if (state === 'now') {
+         this.endTime = new Date()
+         this.setState((prevState) => {
+          return{   state:'waiting',
+             message: ' Click to Start',
+             result: [...prevState.result, this.endTime-this.startTime]
+        
+            } 
+   })    
+   console.log(result)
+       
+         }
+     }
+     onReset = () => {
+        this.setState({
+          result: [],
+        });
+      };
+     renderAverage = () => {
+        
+        const {result} = this.state; 
+        return result.length === 0 ? null : <> <div>Average Response Time: {this.state.result.reduce((a, c) => a + c)/
+        result.length*0.01} sec </div>
+        <button onClick={this.onReset}>리셋</button></>}
+       
+
+    render () 
+    {
+    const {state, message} = this.state;  
+   return (
+    <>
+    <div id="screen" className={state} 
+    onClick={this.onClickScreen}>
+        {message}
+    </div>
+    {this.renderAverage()}
+   
+     </>
+
+        )
+    }
+}
+export default ResponseGame;
